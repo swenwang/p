9ead3ac14060beea6e58323dd706554a428f8ab7
swenwang <wangssuwen588@gmail.com>
2026-09-10 22:56:11 +0800

    Fix navigation on static deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { PortfolioProvider } from './context/PortfolioContext'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PortfolioProvider><App /></PortfolioProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
